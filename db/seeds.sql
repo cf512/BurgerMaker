5a147e8532be723a09564f4612d12358e17f117c
Connor F <connorfinnigan@gmail.com>
2019-04-12 14:12:06 -0500

    initial functionality

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
+INSERT INTO burgers (burger_name) VALUES ('Durward Kirby Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Brisket Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
