aab1ecae921bc78e602138bf9c864bdd4da7be1b
Connor F <connorfinnigan@gmail.com>
2019-04-15 21:57:56 -0500

    updated db files and npm audit fix

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
-INSERT INTO burgers (burger_name) VALUES ('Durward Kirby Burger');
-INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
-INSERT INTO burgers (burger_name) VALUES ('BBQ Brisket Burger');
-INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Patty Melt');
+INSERT INTO burgers (name) VALUES ('Durward Kirby Burger');
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (name) VALUES ('BBQ Brisket Burger');
+INSERT INTO burgers (name) VALUES ('Big Kahuna Burger');
